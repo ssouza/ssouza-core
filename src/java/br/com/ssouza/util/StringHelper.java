70c91d1c5773ac16a680105a86c1bbe7d4fd1eb7
Sérgio Souza <sergio.lcs@gmail.com>
2010-12-27 21:41:25 -0300

    classe de tratamento de strings

---
+++ src/java/br/com/ssouza/util/StringHelper.java
@@ -0,0 +1,44 @@
+package br.com.ssouza.util;
+
+/**
+ * Classe que define métodos para tratamento de strings.
+ * 
+ * @author <a href="mailto:sergio.lcs@gmail.com">Sérgio Souza</a>
+ * @version 1.0
+ * @since Dec 27, 2010
+ */
+public final class StringHelper {
+
+	/**
+	 * Remove tudo que não é número da string.
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public static String removerNaoDigitos(String value) {
+		return value == null ? "" : value.replaceAll("\\D", "");
+	}
+
+	/**
+	 * Verifica se o valor passado é uma sequencia numérica
+	 * 
+	 * @param value
+	 * @return
+	 * @throws NullPointerException
+	 */
+	public static boolean isSequenciaNumerica(String value) {
+
+		if (value.length() < 2) {
+			return false;
+		}
+
+		for (int i = 1; i < value.length(); i++) {
+			if (value.charAt(i) != value.charAt(0)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+}
---
+++ src/test/br/com/ssouza/util/StringHelperTest.java
@@ -0,0 +1,63 @@
+package br.com.ssouza.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+
+	@Test
+	public void testRemoverNaoDigitosNulo() {
+		String retorno = StringHelper.removerNaoDigitos(null);
+		assertNotNull(retorno);
+		assertTrue(retorno.isEmpty());
+	}
+
+	@Test
+	public void testRemoverNaoDigitosVazio() {
+		String retorno = StringHelper.removerNaoDigitos("");
+		assertNotNull(retorno);
+		assertTrue(retorno.isEmpty());
+	}
+
+	@Test
+	public void testRemoverNaoDigitosValido() {
+		String retorno = StringHelper.removerNaoDigitos("abc123#$%");
+		assertNotNull(retorno);
+		assertFalse(retorno.isEmpty());
+		assertEquals("123", retorno);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testIsSequenciaNumericaNulo() {
+		StringHelper.isSequenciaNumerica(null);
+	}
+
+	@Test
+	public void testIsSequenciaNumericaVazio() {
+		boolean valido = StringHelper.isSequenciaNumerica("");
+		assertFalse(valido);
+	}
+
+	@Test
+	public void testIsSequenciaNumerica1Caracter() {
+		boolean valido = StringHelper.isSequenciaNumerica("1");
+		assertFalse(valido);
+	}
+
+	@Test
+	public void testIsSequenciaNumericaNaoValido() {
+		boolean valido = StringHelper.isSequenciaNumerica("123");
+		assertFalse(valido);
+	}
+
+	@Test
+	public void testIsSequenciaNumericaValido() {
+		boolean valido = StringHelper.isSequenciaNumerica("11");
+		assertTrue(valido);
+	}
+
+}
