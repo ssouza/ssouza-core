43458020bf2dbd81e267bef9ba4c62020c2de954
Sérgio Souza <sergio.lcs@gmail.com>
2011-01-19 16:32:35 -0300

    correção no helper de relatorio

--- src/java/br/com/ssouza/web/util/RelatorioHelper.java
+++ src/java/br/com/ssouza/web/util/RelatorioHelper.java
@@ -1,6 +1,7 @@
 package br.com.ssouza.web.util;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.util.Map;
 
@@ -25,7 +26,7 @@ import br.com.ssouza.util.Constantes;
 public final class RelatorioHelper {
 
 	public static void gerarPDF(HttpServletResponse response, Connection conexao, Map<String, Object> map,
-			String caminhoRelatorio, String nomeRelatorio) {
+			String caminhoRelatorioJasper, String nomeRelatorio) {
 
 		if (conexao == null) {
 			throw new NullPointerException("A conexão não pode ser nula.");
@@ -35,15 +36,15 @@ public final class RelatorioHelper {
 			throw new NullPointerException("O map não pode ser nulo.");
 		}
 
-		if (caminhoRelatorio == null) {
+		if (caminhoRelatorioJasper == null) {
 			throw new NullPointerException("O caminho para o relatório não pode ser nulo.");
 		}
 
 		try {
 
-			File file = new File(caminhoRelatorio);
-			if (!file.exists()) {
-				throw new SystemException("O relatório '" + file.getName() + "' não foi encontrado.");
+			File arquivoJasper = new File(caminhoRelatorioJasper);
+			if (!arquivoJasper.exists()) {
+				throw new FileNotFoundException("O relatório '" + arquivoJasper.getName() + "' não foi encontrado.");
 			}
 
 			if (StringUtils.isNotBlank(nomeRelatorio)) {
@@ -54,28 +55,26 @@ public final class RelatorioHelper {
 
 			byte[] bytes = null;
 
-			if (!conexao.isClosed()) {
-
-				map.put(JRParameter.REPORT_LOCALE, Constantes.LOCALE_PT_BR);
-
-				bytes = JasperRunManager.runReportToPdf(file.getPath(), map, conexao);
-
-				response.setContentType("application/pdf");
-				response.setHeader("Pragma", "");
-				response.setHeader("Cache-Control", "");
-				response.setHeader("Expires", "");
-				response.setHeader("Content-Disposition", "inline;" + nomeRelatorio);
-				response.setContentLength(bytes.length);
-
-				ServletOutputStream ouputStream = response.getOutputStream();
-				ouputStream.write(bytes, 0, bytes.length);
-				ouputStream.flush();
-				ouputStream.close();
-
-			} else {
+			if (conexao.isClosed()) {
 				throw new SystemException("Não foi possível obter a conexão com o "
 						+ "Banco de Dados para a geração do Relatório.");
 			}
+
+			map.put(JRParameter.REPORT_LOCALE, Constantes.LOCALE_PT_BR);
+
+			bytes = JasperRunManager.runReportToPdf(arquivoJasper.getPath(), map, conexao);
+
+			response.setContentType("application/pdf");
+			response.setHeader("Pragma", "");
+			response.setHeader("Cache-Control", "");
+			response.setHeader("Expires", "");
+			response.setHeader("Content-Disposition", "inline;" + nomeRelatorio);
+			response.setContentLength(bytes.length);
+
+			ServletOutputStream ouputStream = response.getOutputStream();
+			ouputStream.write(bytes, 0, bytes.length);
+			ouputStream.flush();
+			ouputStream.close();
 
 		} catch (Exception e) {
 			throw new SystemException(e);
